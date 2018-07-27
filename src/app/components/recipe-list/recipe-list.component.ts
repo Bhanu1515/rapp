d1959e627d34331bb88812b33e1ea62e9300e0e7
Bhanu1515 <bhanu2014@gmail.com>
2018-07-27 18:42:22 +0530

    5.1 completed

--- src/app/components/recipe-list/recipe-list.component.ts
+++ src/app/components/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Recipe } from '../..//model/recipe';
+import { JsonPipe } from '../../../../node_modules/@angular/common';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,16 +10,26 @@ import { Recipe } from '../..//model/recipe';
 export class RecipeListComponent {
 
   recipes: Recipe[];
-  
+  recipe_in_progress: Recipe;
+
   constructor() {
+    this.recipe_in_progress = Recipe.creatBlank();
     this.recipes = [
       new Recipe("Matar Paneer Masala", "Talk about veg recipes of India and the first thing that comes to the mind is paneer. The wide use of cottage cheese in Indian kitchen tells how much people love to gorge on it. This Matar Paneer Masala recipe, with a host of spices will leave you asking for more.",
-        null, null, null),
+        4, 30, null, null, null),
       new Recipe("Beguni Recipe", "This Bengali veg recipe of India is super healthy and delicious. It is just the perfect way of eating brinjal for those who don't really like this veggie. It also makes for a street food in most parts in West Bengal. All you need is besan, brinjal, ginger paste, green chilies and khus khus to make this amazing recipe.",
-        null, null, null),
+        2, 50, null, null, null),
       new Recipe("Nuggekai Pulimunchi Recipei", "This veg recipe of India is prepared with chilies and tamarind and makes for a yummy curry. It is generally eaten with rice, Kerala paratha or even with neer dosa.",
-        null, null, null)
+        3, 60, null, null, null)
     ];
+  }
+
+
+  public addRecipeClicked(){
+    console.log(JSON.stringify(this.recipe_in_progress, null, 2));
+    // this.recipes.push(this.recipe_in_progress);
+    this.recipes.unshift(this.recipe_in_progress);
+    this.recipe_in_progress = Recipe.creatBlank();
   }
 
 }
--- src/app/model/recipe.ts
+++ src/app/model/recipe.ts
@@ -11,15 +11,24 @@ export interface Instruction {
 export class Recipe {
     public title: string;
     public description: string;
+    public feeds_this_many: number;   //#ppl
+    public preparation_time: number;  //minutes
     public ingredient: Ingredient[];
     public instruction: Instruction[];
     public cover_photo: string;
 
-    constructor(t: string, d: string, ingr: Ingredient[], instr: Instruction[], cp: string) {
+    constructor(t: string, d: string, feeds: number, pt: number, ingr: Ingredient[], instr: Instruction[], cp: string) {
         this.title = t;
         this.description = d;
+        this.feeds_this_many = feeds;
+        this.preparation_time = pt;
         this.ingredient = ingr;
         this.instruction = instr;
         this.cover_photo = cp;
+    }
+
+    public static creatBlank(){
+        return new Recipe('', '', 1, 1, null, null, null);
+
     }
 }
